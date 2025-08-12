72c8ae1a3b73a4e97755f8fb240ee40826c5dd8b
Alan Melendez <alancuevasmelendezalan123@gmail.com>
2025-08-12 15:46:34 -0600

    Add menu items configuration and implement HomeScreen with AppBar

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class MenuItems {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItems({
+    required this.title,
+    required this.subTitle,
+    required this.link,
+    required this.icon,
+  });
+}
+
+const appMenuItems = <MenuItems>[
+  MenuItems(
+    title: 'Botones',
+    subTitle: 'Varios tipos de botones',
+    link: '/buttons',
+    icon: Icons.smart_button_outlined,
+  ),
+  MenuItems(
+    title: 'Tarjetas',
+    subTitle: 'Contenedor estilizado',
+    link: '/card',
+    icon: Icons.settings,
+  ),
+  MenuItems(
+    title: 'Profile',
+    subTitle: 'View your profile',
+    link: '/profile',
+    icon: Icons.person,
+  ),
+];
