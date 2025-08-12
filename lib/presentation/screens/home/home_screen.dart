336d23cd677d3ae41ed91147134aeb54593a08b1
Alan Melendez <alancuevasmelendezalan123@gmail.com>
2025-08-12 15:58:19 -0600

    Implement HomeView with dynamic menu items in HomeScreen

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,6 +9,27 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Widgets App Flutter'),
+      ),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    var appMenu = appMenuItems;
+
+    return ListView.builder(
+      itemCount: appMenu.length,
+      itemBuilder: (context, index,) => ListTile(
+        title: Text('${appMenu[index].title}'),
+        trailing: Icon(Icons.arrow_forward_ios_outlined),
+        onTap: () {
+          print('Navigate to ${appMenu[index].link}');
+        },
       ),
     );
   }
