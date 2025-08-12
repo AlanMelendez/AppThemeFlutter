f1ad5544050e861bea428ac5b43388796f2c4443
Alan Cuevas <alan.cuevas@grupobatta.com>
2025-08-12 15:29:21 -0600

    Implement theme management with AppTheme class and update main app structure

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.red,
+  Colors.green,
+  Colors.yellow,
+  Colors.purple, 
+  Colors.orange,
+  Colors.cyan,
+  Colors.pink,
+  Colors.teal,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor= 0
+  }): assert(selectedColor >= 0 && selectedColor < colorList.length, 'Selected color must be greater than of 0'); // avoid selection out of list bounds
+
+
+  ThemeData getTheme()=> ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed:   colorList[selectedColor],
+  );
+}
