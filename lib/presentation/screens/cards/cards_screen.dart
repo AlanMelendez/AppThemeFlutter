06a80f339c3b27951de17f82bff1fed7f58c8e95
Alan Melendez <alancuevasmelendezalan123@gmail.com>
2025-08-19 15:34:59 -0600

    Remove excess elevation cards and enhance _CardType1 with an icon button and label display

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -7,16 +7,6 @@ const cards = <Map<String,dynamic>>[
   {'elevation': 3.0, 'label': 'Elevation 3.0'},
   {'elevation': 4.0, 'label': 'Elevation 4.0'}, 
   {'elevation': 5.0, 'label': 'Elevation 5.0'},
-  {'elevation': 6.0, 'label': 'Elevation 6.0'},
-  {'elevation': 7.0, 'label': 'Elevation 7.0'},
-  {'elevation': 8.0, 'label': 'Elevation 8.0'},
-  {'elevation': 9.0, 'label': 'Elevation 9.0'},
-  {'elevation': 10.0, 'label': 'Elevation 10.0'},
-  {'elevation': 11.0, 'label': 'Elevation 11.0'},
-  {'elevation': 12.0, 'label': 'Elevation 12.0'},
-  {'elevation': 13.0, 'label': 'Elevation 13.0'},
-  {'elevation': 14.0, 'label': 'Elevation 14.0'},
-  {'elevation': 15.0, 'label': 'Elevation 15.0'},
 ];
 
 class CardsScreen extends StatelessWidget {
@@ -46,7 +36,8 @@ class _CardsView extends StatelessWidget {
       child: Column(
         children: [
           ...cards.map((card)=> _CardType1(elevation: card['elevation'],label:  card['label'])),
-      
+          ...cards.map((card)=> _CardType2(elevation: card['elevation'],label:  card['label'])),
+
         ],
       ),
     );
@@ -58,6 +49,46 @@ class _CardType1 extends StatelessWidget {
   final String label;
   final double elevation;
   const _CardType1({ required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+        side: BorderSide(color: colors.outline, width: 1),
+      ),
+      elevation: elevation,
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: Icon(Icons.more_vert_outlined),
+                onPressed: (){},
+              )
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - outline'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+
+  final String label;
+  final double elevation;
+  const _CardType2({ required this.label, required this.elevation});
 
   @override
   Widget build(BuildContext context) {
