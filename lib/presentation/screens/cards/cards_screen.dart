22f9f7690de280e4985539472b125acb7fb3c3f5
Alan Melendez <alancuevasmelendezalan123@gmail.com>
2025-08-19 15:50:06 -0600

    Add _CardType3 and _CardType4 to CardsScreen for enhanced card display

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -37,7 +37,10 @@ class _CardsView extends StatelessWidget {
         children: [
           ...cards.map((card)=> _CardType1(elevation: card['elevation'],label:  card['label'])),
           ...cards.map((card)=> _CardType2(elevation: card['elevation'],label:  card['label'])),
+          ...cards.map((card)=> _CardType3(elevation: card['elevation'],label:  card['label'])),
+          ...cards.map((card)=> _CardType4(elevation: card['elevation'],label:  card['label'])),
 
+          SizedBox(height: 50,)
         ],
       ),
     );
@@ -111,6 +114,88 @@ class _CardType2 extends StatelessWidget {
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+
+class _CardType3 extends StatelessWidget {
+
+  final String label;
+  final double elevation;
+  const _CardType3({ required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      color: colors.onSurfaceVariant,
+      elevation: elevation,
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: Icon(Icons.more_vert_outlined),
+                onPressed: (){},
+              )
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - Filled', style: TextStyle(color: Colors.white),),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class _CardType4 extends StatelessWidget {
+
+  final String label;
+  final double elevation;
+  const _CardType4({ required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+
+    // final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      clipBehavior: Clip.hardEdge,
+      elevation: elevation,
+      child: Stack(
+        children: [
+      
+          Image.network(
+            'https://picsum.photos/id/${elevation.toInt()}/300/250',
+            height: 350,
+            fit: BoxFit.cover,
+          ),
+      
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20))
+
+              ),
+              child: IconButton(
+                icon: Icon(Icons.more_vert_outlined),
+                onPressed: (){},
+              ),
+            )
+          ),
+         
+        ],
       ),
     );
   }
