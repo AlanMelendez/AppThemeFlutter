cfcb9b0904f8842b604a53f1f6c64cdafb87148c
Alan Melendez <alancuevasmelendezalan123@gmail.com>
2025-08-21 15:30:28 -0600

    Enhance SnackbarScreen with dialog functionality and update UI layout

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   static const name = 'snackbar_screen';
@@ -21,13 +22,56 @@ class SnackbarScreen extends StatelessWidget {
     ScaffoldMessenger.of(context).showSnackBar(snackbar);
   }
 
+
+  void openDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false, //avoid close if press wiouth dialog.
+      builder: (context) => AlertDialog(
+        title: Text('Dialogo de alerta'),
+        content: Text('Este es el contenido del dialogo de alerta'),
+        actions: [
+          TextButton(
+            onPressed: () => context.pop(),
+            child: Text('Cancelar'),
+          ),
+          FilledButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text('Aceptar'),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: key,
       appBar: AppBar(title: Text('Snackbars & Dialogs')),
       body: Center(
-        child: Text('Presiona el botón para mostrar un Snackbar'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+              onPressed: () {
+                showAboutDialog(
+                  context: context,
+                  children: [
+                    Text('lorem ipsum dolor sit amet, consectetur adipiscing elit.'),
+                  ]
+                );
+              },
+              child: Text("Licencias Usadas"),
+            ),
+            FilledButton.tonal(
+              onPressed: () {
+                openDialog(context);
+              },
+              child: Text("Mostrar Dialogo Pantalla"),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         label: Text("Mostrar Snackbar"),
