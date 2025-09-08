9cf6cb85c746419f05e99249d7108c5deddf4b40
Alan Melendez <alancuevasmelendezalan123@gmail.com>
2025-09-08 15:17:28 -0600

    Refactor AnimatedScreen to use StatefulWidget and implement dynamic shape changes

--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
+import 'dart:math' show Random;
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
   static const name = 'animated_screen';
   const AnimatedScreen({super.key});
+
+  @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+  double _width = 50;
+  double _height = 50;
+
+  Color _color = Colors.blue;
+  double _borderRadius = 10.0;
+
+  void changeShape() {
+    final random = Random();
+
+    _width = random.nextInt(300).toDouble() + 50;
+    _height = random.nextInt(300).toDouble() + 50;
+    _color = Color.fromRGBO(
+      random.nextInt(256),
+      random.nextInt(256),
+      random.nextInt(256),
+      1,
+    );
+    _borderRadius = random.nextInt(100).toDouble() + 20;
+
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,31 +38,22 @@ class AnimatedScreen extends StatelessWidget {
       appBar: AppBar(title: Text("Animated Container")),
 
       body: Center(
-
-         child: AnimatedContainer(
+        child: AnimatedContainer(
           duration: Duration(milliseconds: 300),
           curve: Curves.easeOutCubic,
-          width: 150,
-          height: 150,
+          width: _width <= 0 ? 0 : _width,
+          height: _height <= 0 ? 0 : _height,
           decoration: BoxDecoration(
-            color: Colors.amber,
-            borderRadius: BorderRadius.circular(40)
+            color: _color,
+            borderRadius: BorderRadius.circular(_borderRadius),
           ),
         ),
-
-
-        // child: Container(
-        //   width: 150,
-        //   height: 150,
-        //   decoration: BoxDecoration(
-        //     color: Colors.amber,
-        //     borderRadius: BorderRadius.circular(40)
-        //   ),
-        // ),
       ),
 
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          changeShape();
+        },
         child: Icon(Icons.play_arrow),
       ),
     );
