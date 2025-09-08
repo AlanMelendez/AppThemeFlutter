fe5b06b70ec00219acc8367df420ab1cd83459a3
Alan Melendez <alancuevasmelendezalan123@gmail.com>
2025-09-08 16:06:37 -0600

    Refactor AppTutorialScreen to include SlideInfo class and initialize slides for tutorial content

--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,22 +1,32 @@
 import 'package:flutter/material.dart';
 
+class SlideInfo {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SlideInfo({required this.title, required this.caption, required this.imageUrl});
+
+}
+
+final slides = <SlideInfo> [
+  SlideInfo(title: 'Busca la comida', caption: 'lorem', imageUrl: 'assets/images/1.png'),
+  SlideInfo(title: 'Entrega rapida', caption: 'caption', imageUrl: 'assets/images/1.png'),
+  SlideInfo(title: 'Disfruta la comida', caption: 'caption', imageUrl: 'assets/images/1.png')
+
+];
 
 class AppTutorialScreen extends StatelessWidget {
-
   static const name = 'tutorial_screen';
   const AppTutorialScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+    return Scaffold(
+      body: PageView(
+        physics: BouncingScrollPhysics(), //
+        children: [
+        ],
       ),
     );
   }
