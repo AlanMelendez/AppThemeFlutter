2413c99f3fdea40268ae49756c371fd59e3341da
Alan Melendez <alancuevasmelendezalan123@gmail.com>
2025-09-09 15:36:26 -0600

    Refactor AppTutorialScreen to enhance slide presentation and add skip button; update pubspec.yaml for asset formatting

--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SlideInfo {
   final String title;
   final String caption;
   final String imageUrl;
 
-  SlideInfo({required this.title, required this.caption, required this.imageUrl});
-
+  SlideInfo({
+    required this.title,
+    required this.caption,
+    required this.imageUrl,
+  });
 }
 
-final slides = <SlideInfo> [
-  SlideInfo(title: 'Busca la comida', caption: 'lorem', imageUrl: 'assets/images/1.png'),
-  SlideInfo(title: 'Entrega rapida', caption: 'caption', imageUrl: 'assets/images/1.png'),
-  SlideInfo(title: 'Disfruta la comida', caption: 'caption', imageUrl: 'assets/images/1.png')
-
+final slides = <SlideInfo>[
+  SlideInfo(
+    title: 'Busca la comida',
+    caption: 'lorem',
+    imageUrl: 'assets/images/1.png',
+  ),
+  SlideInfo(
+    title: 'Entrega rapida',
+    caption: 'caption',
+    imageUrl: 'assets/images/2.png',
+  ),
+  SlideInfo(
+    title: 'Disfruta la comida',
+    caption: 'caption',
+    imageUrl: 'assets/images/3.png',
+  ),
 ];
 
 class AppTutorialScreen extends StatelessWidget {
@@ -23,10 +38,65 @@ class AppTutorialScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        physics: BouncingScrollPhysics(), //
+      body: Stack(
         children: [
+          PageView(
+            physics: BouncingScrollPhysics(), //
+            children: slides
+                .map(
+                  (slideData) => _Slide(
+                    title: slideData.title,
+                    caption: slideData.caption,
+                    imageUrl: slideData.imageUrl,
+                  ),
+                )
+                .toList(),
+          ),
+          Positioned(
+           right: 20,
+           top: 50,
+           child: TextButton(
+            child: Text('Skip'),
+            onPressed: ()=> context.pop(),
+           ),
+          )
         ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  const _Slide({
+    required this.title,
+    required this.caption,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+    final captionStyle = Theme.of(context).textTheme.bodySmall;
+
+
+    return Padding(
+      padding: EdgeInsetsGeometry.symmetric(horizontal: 30),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(image: AssetImage(imageUrl)),
+            SizedBox(height: 20),
+            Text(title, style: titleStyle,),
+            SizedBox(height: 10),
+            Text(caption, style: titleStyle,),
+          ],
+        ),
       ),
     );
   }
